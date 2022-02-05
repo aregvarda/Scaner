3932fd67d2fb0b040e8733020440019e8ede14ca
Areg Vardanian <aregv@bk.ru>
2022-02-05 14:36:56 +0300

    finish commit

---
+++ Scaner/View/TextPreviewView.swift
@@ -0,0 +1,28 @@
+//
+//  TextPreviewView.swift
+//  Scaner
+//
+//  Created by Геворг on 05.02.2022.
+//
+
+import SwiftUI
+
+struct TextPreviewView: View {
+    var text: String
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                Text(text)
+                    .font(.body)
+                    .padding()
+            }
+        }
+    }
+}
+
+struct TextPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextPreviewView(text: "")
+    }
+}
