556366708c16b8aaf907498754c90aafdbb5b4c9
Areg Vardanian <aregv@bk.ru>
2022-02-05 12:53:53 +0300

    Initial Commit

---
+++ Scaner/ScanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanerApp.swift
+//  Scaner
+//
+//  Created by Геворг on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
